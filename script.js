5fe19d396f773a3a7aa7f6aa3717306a1f0acd78
Amanjot Singh <98868918+Amanjot-001@users.noreply.github.com>
2023-04-05 14:51:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const giveCode = document.getElementById('message');
+const givenCode = document.getElementById('message');
 const dropDown = document.getElementById('options');
 const button = document.getElementById('convert-btn');
 const res = document.getElementById('result');
-let langToBeConverted = ''
+let langToBeConverted = dropDown.value;
 
 dropDown.addEventListener('change', () => {
      langToBeConverted = dropDown.value;
 })
-
+res.readOnly = true;
 button.addEventListener('click', () => {
+    if(!givenCode.value){
+        res.value = 'type something mf'
+        return;
+    }
     console.log('hi');
-    fetch('http://localhost:8080/', {
+    fetch('http://localhost:5050/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
             lang: langToBeConverted,
-            message: giveCode.value
+            message: givenCode.value
         })
     })
     .then(res => res.json())
