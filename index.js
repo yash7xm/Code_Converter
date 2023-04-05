d8b9a4b35b31ec32019c478d919e807b067e58d2
Amanjot Singh <98868918+Amanjot-001@users.noreply.github.com>
2023-04-05 14:50:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,69 @@ import { Configuration, OpenAIApi } from "openai";
 import express from 'express';
 import bodyParser from "body-parser";
 import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config()
 
 const configuration = new Configuration({
-    apiKey: 
+    apiKey: process.env.API_KEY
 })
 
 const openai = new OpenAIApi(configuration);
 
 const app = express();
-const port = 8080;
+const port = 5050;
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/', async (req,res) => {
     const { lang,message } = req.body;
-
+    let check = ''
     const completion = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [{
             role: 'user', 
-            content: `convert ${message} to ${lang} only provide code and dont write a single other word`
+            content: `'${message}' check this is code or not and only provide one word yes or no`
     }]
     })
-    res.json({
-        completion: completion.data.choices[0].message
+    check = completion.data.choices[0].message.content
+    console.log(check)
+    if(check=='Yes.' || check=='Yes' || check == 'yes'){ 
+        console.log('i am in')
+    const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{
+            role: 'user', 
+            content: `convert ${message} to ${lang} programming language and only provide the converted code and avoid adding any additional text.`
+    }]
     })
+    console.log(completion.data.choices[0].message.content)
+    const finalResponse = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{
+            role: 'user', 
+            content: `${completion.data.choices[0].message.content} check this is code or not and only provide one word yes or no`
+    }]
+    })
+    let final = finalResponse.data.choices[0].message.content
+    console.log(final)
+    if(final == 'Yes.' || final == 'Yes' || final =='yes'){
+        res.json({
+            completion: completion.data.choices[0].message
+        })
+    }
+    else{
+    res.json({
+        completion: {content: `Not Possible to directly convert to ${lang}`}
+    })
+}
+}
+else{
+    console.log("i am in no")
+    res.json({
+        completion: {content: 'Not a valid code'}
+    })
+}
 })
 
 app.listen(port, () =>{
