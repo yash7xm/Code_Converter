f40c57704aeda325540869022f31146d2fe82a5e
yash7xm <yash7xm@gmail.com>
2023-04-06 16:29:00 +0530

    .

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ button.addEventListener('click', () => {
         return;
     }
     // console.log('hi');
-    fetch('http://192.168.1.14:8000/', {
+    fetch('https://code-converter-us52.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
