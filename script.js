9405218f8706225e5a0a08917fd4b64ddee4f934
yash7xm <yash7xm@gmail.com>
2023-04-07 13:57:24 +0530

    update

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ button.addEventListener('click', () => {
         return;
     }
     // console.log('hi');
-    fetch('Access-Control-Allow-Origin','https://codeconverter.onrender.com/', {
+    fetch('https://codeconverter.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
