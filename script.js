26c7dccfc714e9f321e644e171a6cb5d0dd4fc66
yash7xm <yash7xm@gmail.com>
2023-04-07 14:08:15 +0530

    update

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ button.addEventListener('click', () => {
         return;
     }
     // console.log('hi');
-    fetch('https://codeconverter.onrender.com/', {
+    fetch('https://code-converter-us52.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
